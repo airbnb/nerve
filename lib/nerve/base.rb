aad119d6bf99daa75e7a3524692758ef7838b396
Igor Serebryany <igor.serebryany@airbedandbreakfast.com>
2013-01-07 17:00:48 -0800

    base type functionality for nerve, including base class

---
+++ lib/nerve/base.rb
@@ -0,0 +1,18 @@
+
+module Nerve
+  def log(message)
+    @@log = Logger.new STDOUT unless defined?(@@log)
+    @@log.debug message
+  end
+
+  def safe_run(command)
+    res = `#{command}`.chomp
+    raise "command '#{command}' failed to run:\n#{res}" unless $?.success?
+  end
+
+  class Base
+    def initialize
+      super
+    end
+  end
+end
