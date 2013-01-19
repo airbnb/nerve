e357c610ac65ddbc3f08ab6312fc5a371210b6d3
Martin Rhoads <martin.rhoads@airbnb.com>
2013-01-18 17:03:02 -0800

    refactor zk helper

--- lib/nerve/zk_helper.rb
+++ lib/nerve/zk_helper.rb
@@ -1,70 +1,60 @@
 module Nerve
   class ZKHelper
+    include Base
     include Logging
-    def initialize(path)
-      log.debug "creating new zk at #{path}"
-      log.debug "path.class is #{path.class}"
-      log.debug "path.inspect is #{path.inspect}"
-      @zk = ZK.new(path)
-      log.debug "created zk connect to @{path}"
+
+    def initialize(opts)
+      %w{path key}.each do |required|
+        raise ArgumentError, "you need to specify required argument #{required}" unless opts[required]
+        instance_variable_set("@#{required}",opts[required])
+        log.debug "set @#{required} to #{opts[required]}"
+      end
+      save_data(opts['data'] ? opts['data'] : '')
+      @key.insert(0,'/') unless @key[0] == '/'
+
+      log.info "waiting to connect to zookeeper"
+      @zk = ZK.new(@path)
+      log.debug "created zk connection to #{@path}"
     end
 
-    def create_service_node  
-      # create ephemeral node in zookeeper under <root>/services/<service name>/<instance_id>
-      # write json hash {ip:ip, host:host, port:port} into ephemeral node
+    def report_up()
+      zk_save
     end
 
-    def delete(path)
-      log.debug "trying to delete #{path}"
-      @zk.delete(path, :ignore => :no_node)
-      log.debug "path #{path} deleted"
+    def report_down
+      zk_delete
     end
 
-    def create_ephemeral_node(node, data="")
-      node = prepend_slash(node)
-      data = format_data(data)
-      log.debug "creating ephemeral node at '#{node}' with #{data}"
-      @zk.delete(node, :ignore => :no_node)
-      # TODO(mkr): not sure if we should do this, as we are chrooting
-      # to the base dir
-      # create_path(File.dirname(node))
-      @zk.create(node, :data => data.to_json, :mode => :ephemeral)
-    end
-
-    def ensure_ephemeral_node(node,data='')
-      log.debug "ensuring ephemeral node #{node} exists"
-      node = prepend_slash(node)
-      data = format_data(data)
-      @zk.create(node, :data => data.to_json, :mode => :ephemeral, :ignore => :node_exists)
+    def update_data(new_data='')
+      save_data(new_data)
+      zk_save
     end
 
     def ping?
-      #log.debug "pinging zk"
       return @zk.ping?
     end
 
-    def update(node,data='')
-      node = prepend_slash(node)
-      log.debug "updating node #{node}"
-      @zk.set(node,data.to_json)
+    private
+
+    def zk_delete
+      @zk.delete(@key, :ignore => :no_node)
     end
 
-    def format_data(data)
-      return data.to_json if data.class == Hash
-      return data
+    def zk_save
+      begin
+        @zk.set(@key,format_data(data))
+      rescue ZK::Exceptions::NoNode => e
+        @zk.create(node,:data => @data, :mode => :ephemeral)
+      end
     end
 
-    # prepend slash if there is not one already
-    def prepend_slash(path='')
-      path.insert(0,'/') unless path[0] == '/'
-      return path
-    end
-
-    # recursively creates a zk path
-    def create_path(path)
-      return if @zk.exists?(path)
-      create_path File.dirname(path)
-      @zk.create(path)
+    def save_data(data)
+      case data.class
+      when Hash
+        @data = data.to_json
+      else
+        @data = data
+      end
     end
 
   end
