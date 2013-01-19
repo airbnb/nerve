454d2cae84906d0919076bfd86c7311947836f0d
Martin Rhoads <martin.rhoads@airbnb.com>
2013-01-18 17:21:26 -0800

    refactor base module

--- lib/nerve/base.rb
+++ lib/nerve/base.rb
@@ -1,18 +1,44 @@
-
 module Nerve
-  def log(message)
-    @@log = Logger.new STDOUT unless defined?(@@log)
-    @@log.debug message
-  end
+  module Base
+    # def log(message)
+    #   @@log = Logger.new STDOUT unless defined?(@@log)
+    #   @@log.debug message
+    # end
 
-  def safe_run(command)
-    res = `#{command}`.chomp
-    raise "command '#{command}' failed to run:\n#{res}" unless $?.success?
-  end
-
-  class Base
-    def initialize
-      super
+    def safe_run(command)
+      res = `#{command}`.chomp
+      raise "command '#{command}' failed to run:\n#{res}" unless $?.success?
     end
+
+    def ignore_errors(&block)
+      begin
+        yield
+      rescue Object => error
+        log.error "ignoring error #{error.inspect}"
+        return false
+      end
+      return true
+    end
+
+    def retry_this(retry_times=3, &block)
+      retries = 0
+      begin
+        yield
+      rescue Object => e
+        if retries < retry_times
+          log.error "zk_helper encountered an error: #{e.inspect}"
+          log.error "waiting a sec, then retrying..."
+          sleep 1
+          retries += 1
+          retry
+        else
+          log.error "zk_helper encountered a fatal error:"
+          log.error e.inspect
+          log.error e.backtrace
+          raise e
+        end
+      end
+    end
+
   end
 end
