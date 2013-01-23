e73a65266b6f0f831dc0bb4c1e22b5fd55e9b80f
Igor Serebryany <igor.serebryany@airbedandbreakfast.com>
2013-01-22 22:00:38 -0800

    minor logging cleanups

--- lib/nerve/base.rb
+++ lib/nerve/base.rb
@@ -1,10 +1,5 @@
 module Nerve
   module Base
-    # def log(message)
-    #   @@log = Logger.new STDOUT unless defined?(@@log)
-    #   @@log.debug message
-    # end
-
     def safe_run(command)
       res = `#{command}`.chomp
       raise "command '#{command}' failed to run:\n#{res}" unless $?.success?
