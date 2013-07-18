8302d638bb3b61e42205bcce60614ad4eb8a5545
Igor Serebryany <igor.serebryany@airbedandbreakfast.com>
2013-07-18 14:43:10 -0700

    refactor service checks
    get rid of ruby timeout
    use native timeouts for tcp and http checks
    more consistent logging/messaging

--- lib/nerve/service_watcher/tcp.rb
+++ lib/nerve/service_watcher/tcp.rb
@@ -2,31 +2,51 @@ require_relative './base'
 
 module Nerve
   module ServiceCheck
-    require 'socket'
-
     class TcpServiceCheck < BaseServiceCheck
+      require 'socket'
+      include Socket::Constants
+
       def initialize(opts={})
         super
 
-        raise ArgumentError unless opts['port']
+        raise ArgumentError, "missing required argument port in tcp check" unless opts['port']
 
         @port = opts['port']
         @host = opts['host'] || '127.0.0.1'
-        @name = "tcp-#{@host}:#{@port}"
+
+        @address = Socket.sockaddr_in(@port, @host)
       end
 
       def check
-        log.debug "running health check #{@name}"
+        log.debug "nerve: running TCP health check #{@name}"
+
+        # create a TCP socket
+        socket = Socket.new(AF_INET, SOCK_STREAM, 0)
 
         begin
-          socket = TCPSocket.new(@host,@port)
-        rescue
-          return false
+          # open a non-blocking connection
+          socket.connect_nonblock(@address)
+        rescue Errno::EINPROGRESS
+          # opening a non-blocking socket will usually raise
+          # this exception. it's just connect returning immediately,
+          # so it's not really an exception, but ruby makes it into
+          # one. if we got here, we are now free to wait until the timeout
+          # expires for the socket to be writeable
+          IO.select(nil, [socket], nil, @timeout)
+
+          # we should be connected now; allow any other exception through
+          begin
+            socket.connect_nonblock(@address)
+          rescue Errno::EISCONN
+            return true
+          end
+        else
+          # we managed to connect REALLY REALLY FAST
+          log.debug "nerve: connected to non-blocking socket without an exception"
+          return true
         ensure
           socket.close
         end
-
-        return true
       end
     end
 
