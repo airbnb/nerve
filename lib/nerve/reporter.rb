e32f8aa22449e2ab4b1e346a036840c6331bce4a
Pierre Carrier <pierre@gcarrier.fr>
2013-08-23 00:39:42 +0200

    reporter: fix bug around services disappearing after a crash
    Scenario:
    
    Nerve terminates prematurely (maybe because of a disconnection for a ZK
    server).
    
    It gets restarted by runit.
    
    It then does its health checks, finds that the service is up.
    
    It tries to create the node, fails because the node already exists,
    so it sets its value.
    
    The session for the previous instance of nerve expires, and the node
    being ephemeral and attached to that session, disappears.
    
    Nerve doesn't do anything about that unless the service goes down then
    back up, at which point the node gets recreated.

--- lib/nerve/reporter.rb
+++ lib/nerve/reporter.rb
@@ -15,8 +15,15 @@ module Nerve
 
       log.info "nerve: waiting to connect to zookeeper at #{@path}"
       @zk = ZK.new(@path)
-
       log.info "nerve: successfully created zk connection to #{@path}"
+
+      # Get rid of the ephemeral node we don't own,
+      # could be left from a recent crash
+      begin
+        @zk.delete(@key)
+        log.info "nerve: removed stale node #{@key}"
+      rescue ZK::Exceptions::NoNode => e
+      end
     end
 
     #TODO(is): need to check ownership of znodes to resolve name conflicts
