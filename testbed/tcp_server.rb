7190d1834e762e6f5a3d83d1e502f63b6ab9af63
Pierre Carrier <pierre@gcarrier.fr>
2013-08-22 17:48:10 +0200

    testbed/tcp_server.rb: run in a separate thread
    Makes it nice to kill.

--- testbed/tcp_server.rb
+++ testbed/tcp_server.rb
@@ -1,11 +1,13 @@
 #!/usr/bin/env ruby
 require 'socket'
-s = TCPServer.new 4321
-while true
-  begin
-    conn = s.accept
-    eval File.read 'tcp.rb'
-  rescue Exception => e
-    STDERR.puts e
+Thread.start do
+  s = TCPServer.new 4321
+  while true
+    begin
+      conn = s.accept
+      eval File.read 'tcp.rb'
+    rescue Exception => e
+      STDERR.puts e
+    end
   end
-end
+end.join
