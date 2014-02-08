1685062de55d1573608014ac20e532102d14d6d1
Tomas Doran <tdoran@yelp.com>
2014-02-08 21:00:26 +0000

    Move all zookeeper specific initialization to that reporter.
    We're now at the point where all the construction logic is inverted to factories at the variously
    correct levels.
    
    Next steps are to start moving zookeeper specific code out of the base classes

--- lib/nerve/reporter/zookeeper.rb
+++ lib/nerve/reporter/zookeeper.rb
@@ -1,5 +1,13 @@
 class Nerve::Reporter
   class Zookeeper < Base
+    def self.new_from_service(service)
+      self.new({
+        'hosts' => service['zk_hosts'],
+        'path' => service['zk_path'],
+        'key' => "/#{service['instance_id']}_",
+        'data' => {'host' => service['host'], 'port' => service['port'], 'name' => service['instance_id']},
+      })
+    end
   end
 end
 
