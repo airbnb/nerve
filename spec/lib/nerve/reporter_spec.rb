870bd97aaa0dd6e9e3e93669c67bc4e77a0adb72
Tomas Doran <tdoran@yelp.com>
2014-02-08 20:53:35 +0000

    Actually start returning the new class

--- spec/lib/nerve/reporter_spec.rb
+++ spec/lib/nerve/reporter_spec.rb
@@ -21,6 +21,12 @@ describe Nerve::Reporter do
   it 'can really construct an instance of Nerve::Reporter' do
     expect(Nerve::Reporter.new_from_service(subject).is_a?(Nerve::Reporter)).to eql(true)
   end
+  it 'actually constructs an instance of a specific backend' do
+    expect(Nerve::Reporter.new_from_service(subject).is_a?(Nerve::Reporter::Zookeeper)).to eql(true)
+  end
+  it 'the reporter backend inherits from the base class' do
+    expect(Nerve::Reporter.new_from_service(subject).is_a?(Nerve::Reporter::Base)).to eql(true)
+  end
   it 'throws ArgumentError if you ask for a reporter type which does not exist' do
     subject['reporter_type'] = 'does_not_exist'
     expect { Nerve::Reporter.new_from_service(subject) }.to raise_error(ArgumentError)
