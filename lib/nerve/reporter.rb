df3d64d32f0aae8fec2385f37f504bc579b2e60e
Tomas Doran <tdoran@yelp.com>
2014-02-08 22:11:05 +0000

    Add a first initial rough cut of etcd reporter.
    No real tests that this works yet, but it should be basically there..

--- lib/nerve/reporter.rb
+++ lib/nerve/reporter.rb
@@ -15,3 +15,35 @@ module Nerve
     end
   end
 end
+
+class Nerve::Reporter
+  class Base
+    include Nerve::Utils
+    include Nerve::Logging
+    def initialize(opts)
+
+    end
+
+    def start()
+    end
+
+    def report_up()
+    end
+
+    def report_down
+    end
+
+    def update_data(new_data='')
+    end
+
+    def ping?
+    end
+
+    protected
+    def parse_data(data)
+      return data if data.class == String
+      return data.to_json
+    end
+  end
+end
+
