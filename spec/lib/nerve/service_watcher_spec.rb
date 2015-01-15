37902be885774c8920ef35675757dee75196bd17
Igor Serebryany <igor.serebryany@airbedandbreakfast.com>
2015-01-14 18:47:45 -0800

    [spec] tests for the service watcher's main function

--- spec/lib/nerve/service_watcher_spec.rb
+++ spec/lib/nerve/service_watcher_spec.rb
@@ -2,19 +2,29 @@ require 'spec_helper'
 
 describe Nerve::ServiceWatcher do
   describe 'initialize' do
-    let(:service) { build(:service, :zookeeper) }
+    let(:service) { build(:service) }
 
     it 'can successfully initialize' do
       Nerve::ServiceWatcher.new(service)
     end
 
     it 'requires minimum parameters' do
-      %w[zk_hosts zk_path instance_id host port].each do |req|
+      %w[name instance_id host port].each do |req|
         service_without = service.dup
         service_without.delete(req)
 
         expect { Nerve::ServiceWatcher.new(service_without) }.to raise_error
       end
+    end
+  end
+
+  describe 'check_and_report' do
+    let(:service_watcher) { Nerve::ServiceWatcher.new(build(:service)) }
+    let(:reporter) { service_watcher.instance_variable_get(:@reporter) }
+
+    it 'pings the reporter' do
+      expect(reporter).to receive(:ping?)
+      service_watcher.check_and_report
     end
   end
 end
