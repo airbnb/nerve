88d5f9dc3481ee86312824ec3b10938a8cd15407
Rushy Panchal <rushy.panchal@airbnb.com>
2019-09-24 11:04:34 -0700

    Add rate-limiting to Nerve reporting

--- spec/lib/nerve/service_watcher_spec.rb
+++ spec/lib/nerve/service_watcher_spec.rb
@@ -1,7 +1,11 @@
 require 'spec_helper'
 require 'timeout'
+require 'active_support/all'
+require 'active_support/testing/time_helpers'
 
 describe Nerve::ServiceWatcher do
+  include ActiveSupport::Testing::TimeHelpers
+
   describe 'initialize' do
     let(:service) { build(:service) }
 
@@ -60,6 +64,52 @@ describe Nerve::ServiceWatcher do
         end
       end
 
+      context 'when throttled' do
+        before {
+          # Freeze time
+          travel_to Time.now
+
+          allow(reporter).to receive(:ping?).and_return(true)
+          allow(service_watcher).to receive(:check?).and_return(true)
+
+          # 100 is maximum burst
+          for _ in 0..100
+            service_watcher.check_and_report
+            service_watcher.instance_variable_set(:@was_up, false)
+          end
+        }
+
+        it 'still pings reporter' do
+          expect(reporter).to receive(:ping?)
+          service_watcher.check_and_report
+        end
+
+        it 'still checks service health' do
+          expect(reporter).to receive(:ping?).and_return(true)
+          expect(service_watcher).to receive(:check?)
+          service_watcher.check_and_report
+        end
+
+        it 'doesn\'t try to report' do
+          expect(reporter).to receive(:ping?).and_return(true)
+          expect(service_watcher).to receive(:check?).and_return(true)
+
+          expect(reporter).not_to receive(:report_up)
+          expect(reporter).not_to receive(:report_down)
+          expect(service_watcher.check_and_report).to be nil
+        end
+
+        it 'reports new status when no longer throttled' do
+          # Increasing time will remove the throttle
+          travel 1.minute
+
+          expect(reporter).to receive(:ping?).and_return(true)
+          expect(service_watcher).to receive(:check?).and_return(true)
+          expect(reporter).to receive(:report_up).and_return(true)
+          expect(service_watcher.check_and_report).to be true
+        end
+      end
+
       it 'doesn\'t report if the status hasn\'t changed' do
         expect(reporter).to receive(:ping?).and_return(true)
         expect(service_watcher).to receive(:check?).and_return(true)
@@ -69,8 +119,47 @@ describe Nerve::ServiceWatcher do
         expect(reporter).not_to receive(:report_down)
         expect(service_watcher.check_and_report).to be true
       end
-    end
 
+      context 'when the service is flappy' do
+        before {
+          # freeze time
+          travel_to Time.now
+
+          allow(reporter).to receive(:ping?).and_return(true)
+          allow(service_watcher).to receive(:check?) { rand() >= 0.5 }
+
+          for _ in 0..100
+            service_watcher.check_and_report
+          end
+        }
+
+        it 'throttles reporting' do
+          expect(reporter).not_to receive(:report_up)
+          expect(reporter).not_to receive(:report_down)
+          expect(service_watcher.check_and_report).to be nil
+        end
+      end
+
+      context 'when the service is operating normally' do
+        before {
+          travel_to Time.now
+
+          allow(reporter).to receive(:ping?).and_return(true)
+          allow(service_watcher).to receive(:check?).and_return(true)
+
+          for _ in 0..100
+            service_watcher.check_and_report
+          end
+        }
+
+        it 'does not throttle reporting' do
+          # force a new report
+          service_watcher.instance_variable_set(:@was_up, false)
+          expect(reporter).to receive(:report_up).and_return(true)
+          expect(service_watcher.check_and_report).to be true
+        end
+      end
+    end
 
     context 'when pinging of reporter fails' do
       it 'doesn\'t try to report' do
@@ -79,6 +168,12 @@ describe Nerve::ServiceWatcher do
         expect(reporter).not_to receive(:report_down)
 
         expect(service_watcher.check_and_report).to be false
+      end
+
+      it 'does not throttle' do
+        for _ in 0..100 do
+          expect(service_watcher.check_and_report).not_to be nil
+        end
       end
     end
 
